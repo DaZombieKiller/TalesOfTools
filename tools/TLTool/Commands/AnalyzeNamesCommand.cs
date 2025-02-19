1cd18b51b7372656e56a344439e26ffde357218c
Benjamin Moir <DaZombieKiller@users.noreply.github.com>
2025-02-19 14:34:45 +1000

    Bad analyze-names support for ZARC

--- tools/TLTool/Commands/AnalyzeNamesCommand.cs
+++ tools/TLTool/Commands/AnalyzeNamesCommand.cs
@@ -7,7 +7,7 @@ public sealed class AnalyzeNamesCommand
 {
     public Command Command { get; } = new("analyze-names");
 
-    public Argument<string> HeaderPath { get; } = new("header-path", "Path to FILEHEADER.TOFHDB file");
+    public Argument<string> InputPath { get; } = new("input-path", "Path to input file");
 
     public Argument<string> FileDictionaryPath { get; } = new("dictionary", "Path to name dictionary file");
 
@@ -15,24 +15,39 @@ public sealed class AnalyzeNamesCommand
 
     public Option<bool> IsBigEndian { get; } = new("--big-endian", "File is big-endian");
 
+    public Option<bool> IsZarc { get; } = new("--zarc", "Input file is a ZARC");
+
     public AnalyzeNamesCommand()
     {
         Command.AddArgument(FileDictionaryPath);
-        Command.AddArgument(HeaderPath);
+        Command.AddArgument(InputPath);
         Command.AddOption(Is32Bit);
         Command.AddOption(IsBigEndian);
+        Command.AddOption(IsZarc);
         Handler.SetHandler(Command, Execute);
     }
 
     public void Execute(InvocationContext context)
     {
         var data = new TLDataHeader();
-        var mapper = new TLDataNameDictionary();
+        var zarc = new ZArcFile();
         var is32Bit = context.ParseResult.GetValueForOption(Is32Bit);
         var bigEndian = context.ParseResult.GetValueForOption(IsBigEndian);
+        var isZarc = context.ParseResult.GetValueForOption(IsZarc);
+        NameDictionary mapper;
 
-        using (var stream = File.OpenRead(context.ParseResult.GetValueForArgument(HeaderPath)))
-            data.ReadFrom(new BinaryStream(stream, bigEndian), is32Bit);
+        if (isZarc)
+        {
+            zarc.ReadFrom(new FileInfo(context.ParseResult.GetValueForArgument(InputPath)));
+            mapper = new ZArcNameDictionary(zarc.PathCaseConversion);
+        }
+        else
+        {
+            using (var stream = File.OpenRead(context.ParseResult.GetValueForArgument(InputPath)))
+                data.ReadFrom(new BinaryStream(stream, bigEndian), is32Bit);
+
+            mapper = new TLDataNameDictionary();
+        }
 
         mapper.AddNamesFromFile(context.ParseResult.GetValueForArgument(FileDictionaryPath));
         var entriesNamed = 0;
@@ -41,30 +56,52 @@ public sealed class AnalyzeNamesCommand
         var filesByExtension = new Dictionary<string, int>();
         var namedByExtension = new Dictionary<string, int>();
 
-        foreach (var entry in data.Entries)
+        if (mapper is TLDataNameDictionary tldatNames)
         {
-            longestExtension = int.Max(longestExtension, entry.Extension.Length);
-            filesByExtension[entry.Extension] = filesByExtension.GetValueOrDefault(entry.Extension) + 1;
-
-            if (mapper.TryGetValue(entry.NameHash, entry.Extension, out _))
+            foreach (var entry in data.Entries)
             {
-                entriesNamed++;
-                namedByExtension[entry.Extension] = namedByExtension.GetValueOrDefault(entry.Extension) + 1;
+                longestExtension = int.Max(longestExtension, entry.Extension.Length);
+                filesByExtension[entry.Extension] = filesByExtension.GetValueOrDefault(entry.Extension) + 1;
+
+                if (tldatNames.TryGetValue(entry.NameHash, entry.Extension, out _))
+                {
+                    entriesNamed++;
+                    namedByExtension[entry.Extension] = namedByExtension.GetValueOrDefault(entry.Extension) + 1;
+                }
+            }
+        }
+        else if (mapper is ZArcNameDictionary zarcNames)
+        {
+            foreach (var entry in zarc.Entries)
+            {
+                if (zarcNames.TryGetValue(entry.NameHash, out _))
+                {
+                    entriesNamed++;
+                }
             }
         }
 
-        var format = $"{{0,{longestExtension}}} | {{1,{longestCount}}} | {{2,{longestCount}}} | {{3,7}}";
-        var header = string.Format(format, "Type", "Count", "Named", "Percentage");
-        Console.WriteLine(header);
-        Console.WriteLine(new string('-', header.Length));
-
-        foreach (var (extension, fileCount) in filesByExtension.OrderBy(pair => pair.Key))
+        if (isZarc)
+            Console.WriteLine("{0} | {1} | {2} | {3}", "Total", zarc.Entries.Count, entriesNamed, $"{entriesNamed / (float)zarc.Entries.Count * 100:F2}%");
+        else
         {
-            var named = namedByExtension.GetValueOrDefault(extension);
-            Console.WriteLine(format, extension, fileCount, named, $"{named / (float)fileCount * 100:F2}%");
-        }
+            var format = $"{{0,{longestExtension}}} | {{1,{longestCount}}} | {{2,{longestCount}}} | {{3,7}}";
+            var header = string.Format(format, "Type", "Count", "Named", "Percentage");
 
-        Console.WriteLine(new string('-', header.Length));
-        Console.WriteLine(format, "Total", data.Entries.Count, entriesNamed, $"{entriesNamed / (float)data.Entries.Count * 100:F2}%");
+            Console.WriteLine(header);
+            Console.WriteLine(new string('-', header.Length));
+
+            if (!isZarc)
+            {
+                foreach (var (extension, fileCount) in filesByExtension.OrderBy(pair => pair.Key))
+                {
+                    var named = namedByExtension.GetValueOrDefault(extension);
+                    Console.WriteLine(format, extension, fileCount, named, $"{named / (float)fileCount * 100:F2}%");
+                }
+            }
+
+            Console.WriteLine(new string('-', header.Length));
+            Console.WriteLine(format, "Total", data.Entries.Count, entriesNamed, $"{entriesNamed / (float)data.Entries.Count * 100:F2}%");
+        }
     }
 }
