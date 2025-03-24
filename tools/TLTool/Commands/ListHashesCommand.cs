3c00b7fc4babb9516a6138003ebe827530f31444
Benjamin Moir <DaZombieKiller@users.noreply.github.com>
2025-03-25 07:49:41 +1000

    Add basic zarc support to list-hashes

--- tools/TLTool/Commands/ListHashesCommand.cs
+++ tools/TLTool/Commands/ListHashesCommand.cs
@@ -11,21 +11,35 @@ public sealed class ListHashesCommand
 
     public Option<bool> CaseSensitive { get; } = new("--case-sensitive", "Whether to use case-sensitive hashes (Zestiria SCPACK)");
 
+    public Option<bool> Zarc { get; } = new("--zarc", "Whether to use ZARC lowercase hashes");
+
     public ListHashesCommand()
     {
         Command.AddArgument(Names);
         Command.AddOption(CaseSensitive);
+        Command.AddOption(Zarc);
         Handler.SetHandler(Command, Execute);
     }
 
     public void Execute(InvocationContext context)
     {
+        bool isZarc = context.ParseResult.GetValueForOption(Zarc);
         bool ignoreCase = !context.ParseResult.GetValueForOption(CaseSensitive);
         var flags = ignoreCase ? TLHashOptions.IgnoreCase : TLHashOptions.None;
 
-        foreach (string name in context.ParseResult.GetValueForArgument(Names))
+        if (isZarc)
         {
-            Console.WriteLine($"{TLHash.HashToUInt32(name, flags):X8}: {name}");
+            foreach (string name in context.ParseResult.GetValueForArgument(Names))
+            {
+                Console.WriteLine($"{ZArcHash.HashToUInt64(name.ToLowerInvariant()):X16}: {name}");
+            }
+        }
+        else
+        {
+            foreach (string name in context.ParseResult.GetValueForArgument(Names))
+            {
+                Console.WriteLine($"{TLHash.HashToUInt32(name, flags):X8}: {name}");
+            }
         }
     }
 }
